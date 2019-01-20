03e0cae576554658abb6002cf228db9e43b289a5
David Paiva <pillgp@icloud.com>
2019-01-20 18:37:24 +0000

    oops

--- country/assets/index.js
+++ country/assets/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     $('.btn').click(function() {
-        $.get('https://ip-api.com/json/')
+        $.get('https://freegeoip.app/json/')
             .then(data => {
                 console.log(data)
-                $('div.output').html(`<img src="https://raw.githubusercontent.com/yusufshakeel/mysql-country-with-flag/master/flags-medium/${data.countryCode.toLowerCase()}.png"></img>
-                <br><br><h1>${data.country}</h1>
-                <br><h5>${data.query} // ${data.as}</h5>`)
+                $('div.output').html(`<img src="https://raw.githubusercontent.com/yusufshakeel/mysql-country-with-flag/master/flags-medium/${data.country_code.toLowerCase()}.png"></img>
+                <br><br><h1>${data.country_name}</h1>
+                <br><h5>${data.ip} // ${data.time_zone}</h5>`)
             })
     })
 }); 
