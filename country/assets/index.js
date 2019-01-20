3763b1f0e6c8ef069456b907ac4bcae175c9049d
David Paiva <pillgp@icloud.com>
2019-01-20 18:30:40 +0000

    oops

--- country/assets/index.js
+++ country/assets/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.btn').click(function() {
-        $.get('http://ip-api.com/json/')
+        $.get('https://ip-api.com/json/')
             .then(data => {
                 console.log(data)
                 $('div.output').html(`<img src="https://raw.githubusercontent.com/yusufshakeel/mysql-country-with-flag/master/flags-medium/${data.countryCode.toLowerCase()}.png"></img>
