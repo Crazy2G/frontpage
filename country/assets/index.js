280021c1f0363d76c2dac219a926bb8138f35085
David Paiva <pillgp@icloud.com>
2019-01-20 18:37:52 +0000

    oops

--- country/assets/index.js
+++ country/assets/index.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
                 console.log(data)
                 $('div.output').html(`<img src="https://raw.githubusercontent.com/yusufshakeel/mysql-country-with-flag/master/flags-medium/${data.country_code.toLowerCase()}.png"></img>
                 <br><br><h1>${data.country_name}</h1>
-                <br><h5>${data.ip} // ${data.time_zone}</h5>`)
+                <h5>${data.ip} - ${data.time_zone}</h5>`)
             })
     })
 }); 
